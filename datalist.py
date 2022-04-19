c2a1445ce548cfc6432fe100d20e3ddfe8337a58
Tushar Jain <tushar00jain@users.noreply.github.com>
2022-04-19 13:47:58 -0700

    fix

--- datalist.py
+++ datalist.py
@@ -3,7 +3,7 @@ import glob
 
 from pathlib import Path
 
-input_frames = sorted(glob.glob("/data/SOTIS/Stabilized/EurasianCitiesBase/NFrames50/TV-TVL1/**/**/*.png"))
+input_frames = sorted(glob.glob("/data/SOTIS/Stabilized/EurasianCitiesBase/NFrames50/TV-LK/**/**/*.png"))
 
 print(len(input_frames))
 result = ""
@@ -15,6 +15,6 @@ for input_frame in input_frames:
     gt = os.path.join("/data/SOTIS/Groundtruth/EurasianCitiesGT", image + ".png")
     result += gt + " " + input_frame + "\n"
 
-f = open("datalist_sotis_tv_tvl1.txt", "a")
+f = open("datalist_sotis_tv_lk.txt", "w")
 f.write(result)
 f.close()
