23a7580730554d62c9105b44434314a25ec0ce2c
Tushar Jain <tushar00jain@users.noreply.github.com>
2022-04-19 19:20:57 -0700

    update

--- train-aws.sh
+++ train-aws.sh
@@ -2,4 +2,5 @@
 
 pip install pathlib
 python datalist.py
-python run_model.py --datalist /workdir/datalist_sotis_tv_lk.txt --phase=train --batch_size=10 --lr=1e-4 --epoch=311 --model sotis
+python run_model.py --datalist /workdir/datalist_sotis_tv_tvl1.txt --phase=train --batch_size=10 --lr=1e-4 --epoch=311 --model sotis
+
