6c9b70edd9dd5cfbc145be2b3957ce2f4edc1600
Tushar Jain <tushar00jain@users.noreply.github.com>
2022-04-19 19:20:20 -0700

    update

--- entrypoint-test.sh
+++ entrypoint-test.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+set -euxo pipefail
+
 python run_model.py \
-	--input_path=/data/SOTIS2/Stabilized --output_path=/workdir/results --height 256 --width 256 --model sotis
+	--input_path=/data/SOTIS/Stabilized/EurasianCitiesBase/NFrames50/TV-LK --output_path=/workdir/results-pretraining --height 256 --width 256 --model gray
+
+python run_model.py \
+	--input_path=/data/SOTIS/Stabilized/EurasianCitiesBase/NFrames50/TV-TVL1 --output_path=/workdir/results-pretraining --height 256 --width 256 --model gray
+
+# python run_model.py \
+# 	--input_path=/data/SOTIS/Stabilized/EurasianCitiesBase/NFrames50/TV-LK --output_path=/workdir/results-retraining --height 256 --width 256 --model sotis
+
+# python run_model.py \
+# 	--input_path=/data/SOTIS/Stabilized/EurasianCitiesBase/NFrames50/TV-TVL1 --output_path=/workdir/results-retraining --height 256 --width 256 --model sotis
